a850d9616e2f8f8417e9b6cc65e6db836e76d8ed
Flavio Canedo <flavio_canedo@live.com>
2018-12-22 17:56:28 -0200

    class 75

--- cmp-databinding-start/src/app/cockpit/cockpit.component.ts
+++ cmp-databinding-start/src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,5 @@
-import { OnInit, Component, EventEmitter, Output } from "@angular/core";
+import { OnInit, Component, EventEmitter, Output, ViewChild, ElementRef } from "@angular/core";
+import { debug } from "util";
 
 @Component({
     selector: 'app-cockpit',
@@ -9,25 +10,24 @@ import { OnInit, Component, EventEmitter, Output } from "@angular/core";
 export class CockpitComponent implements OnInit {
     @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
     @Output() blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+    @ViewChild('serverNameInput') serverNameInput: ElementRef;
+    @ViewChild('serverContentInput') serverContentInput: ElementRef;
     
-    newServerName = '';
-    newServerContent = '';
-
     constructor() {}
 
     ngOnInit() {}
 
     onAddServer() {
       this.serverCreated.emit({
-        serverName: this.newServerName, 
-        serverContent: this.newServerContent
+        serverName: this.serverNameInput.nativeElement.value, 
+        serverContent: this.serverContentInput.nativeElement.value
       });        
     }
     
     onAddBlueprint() {
       this.blueprintCreated.emit({
-        serverName: this.newServerName, 
-        serverContent: this.newServerContent
+        serverName: this.serverNameInput.nativeElement.value, 
+        serverContent: this.serverContentInput.nativeElement.value
       });
     }
 }
